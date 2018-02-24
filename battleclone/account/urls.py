9571270137fdd1b108faaf29c3cd4c0b4cecc19a
Zawadeusz <zawadeusz@localhost.localdomain>
2018-02-24 19:14:53 +0100

    Add signup link

--- battleclone/account/urls.py
+++ battleclone/account/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
+from . import views
 from django.contrib import admin
 
 urlpatterns = [
-    url('^', include('django.contrib.auth.urls'))
+    url('^', include('django.contrib.auth.urls')),
+    url('^singup/$', views.signup, name='signup'),
 ]
