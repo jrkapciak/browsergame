68de62fa1f04f912931671a9e7f1a84c746637cc
Zawadeusz <zawadeusz@localhost.localdomain>
2018-03-01 21:57:41 +0100

    Add health manager

--- battleclone/character/managers/health.py
+++ battleclone/character/managers/health.py
@@ -1,5 +1,26 @@
 
-
 class HealthManager:
     """ Class responsible for character health managing"""
-    pass
+
+    def __init__(self, character):
+        self.character = character
+        self.actual_health = self.character.health
+
+    def on_drink_health_potion(self, health_potion):
+        self.actual_health += health_potion.value
+        self.save_character()
+
+    def on_regeneration(self, value):
+        self.actual_health += value
+        self.save_character()
+
+    def can_fight(self):
+        # TODO: should move constants values to something like configs
+        return True if self.actual_health > 50 else False
+
+    def is_dead(self):
+        return True if self.actual_health < 1 else False
+
+    def save_character(self):
+        self.character.health = self.actual_health
+        self.character.save()
