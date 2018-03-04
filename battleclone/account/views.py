4551233b95855164e486337904d93db52f79b4b7
Zawadeusz <zawadeusz@localhost.localdomain>
2018-03-04 13:52:32 +0100

    Add automatically generating user profile

--- battleclone/account/views.py
+++ battleclone/account/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, reverse
 from django.views.generic import CreateView, UpdateView
 from .forms import SignUpForm
 from .models import UserProfile
+from battleclone.character.models import Character, Parameters
 
 
 class RegistrationView(CreateView):
@@ -18,12 +19,31 @@ class RegistrationView(CreateView):
 
         return super().dispatch(request, *args, **kwargs)
 
+    @classmethod
+    def create_character(cls, new_user):
+        parameters = Parameters.objects.create()
+
+        return Character.objects.create(
+            nickname=new_user.username, parameters=parameters
+        )
+
+    @classmethod
+    def create_profile(cls, new_user, new_character):
+        UserProfile.objects.create(
+            user=new_user, character=new_character
+        )
+
     def form_valid(self, form):
         new_user = form.save(commit=False)
         new_user.set_password(form.cleaned_data['password'])
         new_user.save()
 
+        new_character = self.create_character(new_user)
+
+        self.create_profile(new_user, new_character)
+
         return super().form_valid(form)
+
 
 
 class EditProfileView(UpdateView):
