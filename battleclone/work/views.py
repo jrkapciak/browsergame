f3f16341651d9f8aa9136eda7846aff2ac0b8283
Zawadeusz <zawadeusz@localhost.localdomain>
2018-03-18 19:04:34 +0100

    Fix bug with no object.pk

--- battleclone/work/views.py
+++ battleclone/work/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.views.generic import FormView
 from django.contrib.auth.decorators import login_required
 from .forms import WorkModelForm
+from battleclone.account.models import UserProfile
 
 
 class WorkView(FormView):
     template_name = 'work/work_view.html'
     form_class = WorkModelForm
 
-
-
-
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['object'] = UserProfile.objects.get(user=self.request.user)
+        return context
